0ac41625a46ae06ae660c0c02b2a26ddd0eb8c2f
Claude Abounegm <claude.abounegm@gmail.com>
2020-01-15 11:12:44 +0200

    modified db and fix pagination
    * fixed pagination on pages with no items
    * added an empty category to db

--- src/components/pagination.jsx
+++ src/components/pagination.jsx
@@ -6,9 +6,11 @@ const Pagination = props => {
 
   const pagesCount = Math.ceil(itemsCount / pageSize);
 
-  if (pagesCount <= 1) {
-    return null;
-  }
+  console.log(itemsCount);
+
+  // if (pagesCount <= 1) {
+  //   return null;
+  // }
 
   function paginationButton(page, opts) {
     const { text, disabled, onPageChange, key } = opts || {};
@@ -27,12 +29,15 @@ const Pagination = props => {
   let pagination = [
     paginationButton(null, {
       key: "previous",
-      disabled: currentPage === 1,
+      disabled: itemsCount === 0 || currentPage === 1,
       text: "← Previous",
       onPageChange: () => onPageChange(currentPage - 1)
     }),
     ..._.range(1, Math.min(pagesCount + 1, 10)).map(page =>
-      paginationButton(page, { disabled: currentPage === page, onPageChange })
+      paginationButton(page, {
+        disabled: currentPage === page,
+        onPageChange
+      })
     )
   ];
 
@@ -40,15 +45,20 @@ const Pagination = props => {
     pagination.push(
       <span key="seperator">...</span>,
       ..._.range(pagesCount - 1, pagesCount + 1).map(page =>
-        paginationButton(page, { disabled: currentPage === page, onPageChange })
+        paginationButton(page, {
+          disabled: currentPage === page,
+          onPageChange
+        })
       )
     );
+  } else if (itemsCount === 0) {
+    pagination.push(paginationButton(1, { disabled: true }));
   }
 
   pagination.push(
     paginationButton(null, {
       key: "next",
-      disabled: currentPage === pagesCount,
+      disabled: itemsCount === 0 || currentPage === pagesCount,
       text: "Next →",
       onPageChange: () => onPageChange(currentPage + 1)
     })
