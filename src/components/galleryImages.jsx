4258ed4614ae1c837eca1e6563a29d99fe6ddcff
Claude Abounegm <claude.abounegm@gmail.com>
2020-01-15 09:52:38 +0200

    pagination now works correctly

--- src/components/galleryImages.jsx
+++ src/components/galleryImages.jsx
@@ -1,11 +1,15 @@
 import React, { useState, useEffect } from "react";
+import qs from "querystring";
 import GalleryImage from "./GalleryImage";
 import Pagination from "./Pagination";
 import http from "../services/httpService";
 import { parseQueryString, maybeParseInt } from "../utils";
+import { Redirect } from "react-router-dom";
 
 const GalleryImages = props => {
-  const { page, limit } = parseQueryString(props.location.search, data => {
+  const parsedQueryString = parseQueryString(props.location.search);
+
+  const { page, limit } = (data => {
     const { page, limit } = data;
 
     return {
@@ -13,23 +17,47 @@ const GalleryImages = props => {
       page: maybeParseInt(page) || 1,
       limit: maybeParseInt(limit) || 9
     };
-  });
+  })(parsedQueryString);
 
   const [images, setImages] = useState([]);
   const [allImages, setAllImages] = useState([]);
+  const [isValidPage, setIsValidPage] = useState(true);
+
+  function buildQueryString(page, limit) {
+    let query = { page };
+    if (limit) {
+      query.limit = limit;
+    }
+    query = qs.stringify(query);
+    return query;
+  }
+
+  function handlePageChange(page, replace) {
+    const query = buildQueryString(page, parsedQueryString.limit);
+    props.history[replace ? "replace" : "push"](`/?${query}`);
+  }
 
   useEffect(() => {
-    console.log("refresh images");
-    http.getImages({ page, limit }).then(setImages);
+    if (isValidPage) {
+      http.getImages({ page, limit }).then(setImages);
+    }
+  }, [page, limit, isValidPage]);
+
+  useEffect(() => {
+    // console.log("refresh all images");
+    http.getImages().then(allImages => {
+      const totalPages = Math.ceil(allImages.length / limit);
+      const isValid = page >= 1 && page <= totalPages;
+
+      setIsValidPage(isValid);
+      if (isValid) {
+        setAllImages(allImages);
+      }
+    });
   }, [page, limit]);
 
-  useEffect(() => {
-    console.log("refresh all images");
-    http.getImages().then(setAllImages);
-  }, []);
-
-  function handlePageChange(page) {
-    props.history.push(`/?page=${page}`);
+  if (!isValidPage) {
+    return <Redirect to="/?page=1" />;
   }
 
   return (
