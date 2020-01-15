63757167920ebb01c08d2636d07786482f6ba242
Claude Abounegm <claude.abounegm@gmail.com>
2020-01-15 06:09:36 +0200

    Adding initial implementation

---
+++ src/components/image.jsx
@@ -0,0 +1,17 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import React from "react";
+
+const Thumbnail = props => {
+  const { src, title, alt } = props;
+
+  return (
+    <div className="thumnail">
+      <div className="bg-img">
+        <img src={src} alt={alt || title}></img>
+      </div>
+      <h3>{title}</h3>
+    </div>
+  );
+};
+
+export default Thumbnail;
