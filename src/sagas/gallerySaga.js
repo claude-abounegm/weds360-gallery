48bdef07a949d92fdec408587345776aa07bebbd
Claude Abounegm <claude.abounegm@gmail.com>
2020-01-18 15:52:48 +0200

    changing default gallerySlice and adding errors

--- src/sagas/gallerySaga.js
+++ src/sagas/gallerySaga.js
@@ -4,6 +4,7 @@ import { loadImages, setGalleryData } from "../features/gallerySlice";
 import { setLoading } from "../features/loadingSlice";
 import { setError } from "../features/errorSlice";
 import http from "../services/httpService";
+import { InvalidPageError } from "../errors";
 
 function putAll(actions) {
   return all(actions.map(action => put(action)));
@@ -12,29 +13,25 @@ function putAll(actions) {
 export function* handleImagesLoad() {
   const { opts } = yield select(({ gallery }) => gallery);
 
-  function throwInvalidPage() {
-    throw new Error("Invalid page");
-  }
-
   try {
     yield put(setLoading(true));
     const { page } = opts;
 
     if (page < 1) {
-      throwInvalidPage();
+      throw new InvalidPageError();
     }
 
     const { images, totalPages } = yield call(http.getImages, opts);
 
     if (totalPages === null) {
-      throwInvalidPage();
+      throw new InvalidPageError();
     }
 
     yield putAll([setError(null), setGalleryData({ images, totalPages })]);
   } catch (error) {
     const err = { message: error.message };
 
-    if (error.message.includes("Invalid page")) {
+    if (error instanceof InvalidPageError) {
       err.to = 1;
     }
 
