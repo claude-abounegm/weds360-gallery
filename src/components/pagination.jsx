4fa8117831936262ef9af2bcf9b7c9114fba0436
Claude Abounegm <claude.abounegm@gmail.com>
2020-01-18 17:10:15 +0200

    adding style to pagination

--- src/components/pagination.jsx
+++ src/components/pagination.jsx
@@ -1,6 +1,61 @@
 import _ from "lodash";
 import React from "react";
 
+import styled from "styled-components";
+
+const PaginationButton = styled.button`
+  position: relative;
+  float: left;
+  padding: 6px 12px;
+  line-height: 1.428571429;
+  text-decoration: none;
+  color: black;
+  background-color: #fff;
+  border: 1px solid #ddd;
+  margin-left: -1px;
+
+  &.active {
+    background-color: #024d4c;
+    border-color: #024d4c;
+    color: white;
+  }
+
+  &:disabled:not(.active) {
+    cursor: not-allowed;
+    color: #777777;
+  }
+
+  &:hover:disabled:not(.active) {
+    color: #777777;
+    background-color: #fff;
+  }
+
+  &:hover:not(.active):not(:disabled) {
+    cursor: pointer;
+    color: #23527c;
+    background-color: #eeeeee;
+    border-color: #ddd;
+  }
+`;
+
+const PaginationPreviousButton = styled(PaginationButton)`
+  border-bottom-left-radius: 4px;
+  border-top-left-radius: 4px;
+`;
+
+const PaginationNextButton = styled(PaginationButton)`
+  border-bottom-right-radius: 4px;
+  border-top-right-radius: 4px;
+`;
+
+const PaginationList = styled.ul`
+  padding-bottom: 40px;
+`;
+
+const PaginationItem = styled.li`
+  display: inline;
+`;
+
 const Pagination = props => {
   const {
     pagesCount = 1,
@@ -15,16 +70,24 @@ const Pagination = props => {
   }
 
   function paginationButton(page, opts) {
-    const { text, disabled, onPageChange, key } = opts || {};
+    const {
+      text,
+      disabled,
+      onPageChange,
+      key,
+      DefaultButton = PaginationButton
+    } = opts || {};
 
     return (
-      <button
-        disabled={disabled}
-        key={key || page}
-        onClick={() => onPageChange(page)}
-      >
-        {text || page}
-      </button>
+      <PaginationItem key={key || page}>
+        <DefaultButton
+          className={currentPage === page ? "active" : ""}
+          disabled={disabled}
+          onClick={() => onPageChange(page)}
+        >
+          {text || page}
+        </DefaultButton>
+      </PaginationItem>
     );
   }
 
@@ -33,7 +96,8 @@ const Pagination = props => {
       key: "previous",
       disabled: currentPage === 1,
       text: "← Previous",
-      onPageChange: () => onPageChange(currentPage - 1)
+      onPageChange: () => onPageChange(currentPage - 1),
+      DefaultButton: PaginationPreviousButton
     }),
     ..._.range(1, Math.min(pagesCount + 1, pagesCountLeft + 1)).map(page =>
       paginationButton(page, {
@@ -48,7 +112,9 @@ const Pagination = props => {
     const start = end - pagesCountRight;
 
     pagination.push(
-      <span key="seperator">...</span>,
+      <PaginationButton disabled key="seperator">
+        ...
+      </PaginationButton>,
       ..._.range(start, end).map(page =>
         paginationButton(page, {
           disabled: currentPage === page,
@@ -63,11 +129,16 @@ const Pagination = props => {
       key: "next",
       disabled: currentPage === pagesCount,
       text: "Next →",
-      onPageChange: () => onPageChange(currentPage + 1)
+      onPageChange: () => onPageChange(currentPage + 1),
+      DefaultButton: PaginationNextButton
     })
   );
 
-  return <nav>{pagination}</nav>;
+  return (
+    <nav>
+      <PaginationList>{pagination}</PaginationList>
+    </nav>
+  );
 };
 
 export default Pagination;
