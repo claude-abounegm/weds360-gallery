a09cfb8bf82abb3b1437e058fb2ac4d32c46ef7e
Claude Abounegm <claude.abounegm@gmail.com>
2020-01-19 04:34:36 +0200

    footer is now styled

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,58 +1,141 @@
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import styled from "styled-components";
 
 import Anchor from "./../elements/Anchor";
 
-const FontAwesomeIconBrand = ({ icon }) => (
-  <FontAwesomeIcon icon={["fab", icon]} />
+const FontAwesomeIconBrand = ({ icon, ...props }) => (
+  <FontAwesomeIcon {...props} icon={["fab", icon]} />
 );
 
-const FacebookIcon = () => <FontAwesomeIconBrand icon="facebook-f" />;
-const InstagramIcon = () => <FontAwesomeIconBrand icon="instagram" />;
+const FacebookIcon = props => (
+  <FontAwesomeIconBrand {...props} icon="facebook-f" />
+);
+const InstagramIcon = props => (
+  <FontAwesomeIconBrand {...props} icon="instagram" />
+);
+
+const FooterElement = styled.footer`
+  font-family: "Lato", sans-serif;
+  display: block;
+  background-color: #000000;
+  width: 100%;
+  direction: ltr !important;
+  color: white;
+`;
+
+const TextLeft = styled.div`
+  box-sizing: border-box;
+  display: inline-block;
+  width: 33%;
+  font-size: 8px;
+  margin-left: 1%;
+`;
+
+const TextCenter = styled.div`
+  box-sizing: border-box;
+  display: inline-block;
+  width: 33%;
+  font-size: 10px;
+  font-weight: 100;
+  color: #ffffff;
+  position: relative;
+  margin: auto;
+  text-align: center;
+
+  & a {
+    font-size: 10px;
+  }
+`;
+
+const TextRight = styled.div`
+  box-sizing: border-box;
+  display: inline-block;
+  width: 31%;
+  color: #ffffff;
+  font-size: 10px;
+  text-decoration: none;
+  padding-right: 60px;
+
+  & ul {
+    text-align: right;
+    list-style-type: none;
+
+    & li {
+      padding-bottom: 4px;
+    }
+
+    & a {
+      font-size: 10px;
+    }
+  }
+`;
+
+const StaticAnchor = styled(Anchor)`
+  color: white;
+
+  &:hover,
+  &:focus {
+    color: white;
+  }
+`;
+
+const SocialMediaLinks = styled.div`
+  font-size: 15px;
+
+  & ${StaticAnchor} {
+    margin-right: 5px;
+  }
+`;
+
+const Strong = styled.strong`
+  font-weight: bold;
+`;
 
 const Footer = () => {
   return (
-    <footer id="footer">
-      <div className="footer--background">
-        <div className="text--left">
-          <h1 className="company--logo">Weds360</h1>
-          <div className="social--media--links">
-            <span>
-              <Anchor href="https://www.facebook.com/Weds360/">
-                <FacebookIcon />
-              </Anchor>
-            </span>
+    <FooterElement>
+      <TextLeft>
+        <h1 className="company--logo">Weds360</h1>
+        <SocialMediaLinks>
+          <span>
+            <StaticAnchor href="https://www.facebook.com/Weds360/">
+              <FacebookIcon size="lg" />
+            </StaticAnchor>
+          </span>
 
-            <span>
-              <Anchor href="https://www.instagram.com/weds360/">
-                <InstagramIcon />
-              </Anchor>
-            </span>
-          </div>
-        </div>
-        <div className="text--center">
-          © Copyright
-          <Anchor href="http://plus360.xyz/">
-            <strong>Plus360</strong>.
-          </Anchor>
-          All Rights Reserved
-        </div>
-        <div className="text--right">
-          <ul>
-            <li>
-              <Anchor href="/pAnchorges/terms-conditions?locale=en">
-                Terms &amp; Conditions
-              </Anchor>
-            </li>
-            <li>
-              <Anchor href="/pages/privacy-policy?locale=en">
-                Privacy Policy
-              </Anchor>
-            </li>
-          </ul>
-        </div>
-      </div>
-    </footer>
+          <span>
+            <StaticAnchor href="https://www.instagram.com/weds360/">
+              <InstagramIcon size="lg" />
+            </StaticAnchor>
+          </span>
+        </SocialMediaLinks>
+      </TextLeft>
+
+      <TextCenter>
+        © Copyright{" "}
+        <Anchor href="http://plus360.xyz/">
+          <Strong>Plus360</Strong>.{" "}
+        </Anchor>
+        All Rights Reserved
+      </TextCenter>
+
+      <TextRight>
+        <ul>
+          <li>
+            <StaticAnchor href="/pages/terms-conditions?locale=en">
+              Terms &amp; Conditions
+            </StaticAnchor>
+          </li>
+
+          <li>
+            <StaticAnchor href="/pages/privacy-policy?locale=en">
+              Privacy Policy
+            </StaticAnchor>
+          </li>
+        </ul>
+      </TextRight>
+    </FooterElement>
   );
 };
 
