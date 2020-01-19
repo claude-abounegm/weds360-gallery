a159bfa50029c6a6af494fb1c8f44bf5487fe41c
Claude Abounegm <claude.abounegm@gmail.com>
2020-01-19 06:06:43 +0200

    cleaning up , added breadcrumbs to image

--- src/components/image.jsx
+++ src/components/image.jsx
@@ -1,7 +1,12 @@
 import React, { useEffect, useState } from "react";
+import { connect } from "react-redux";
+
 import http from "../services/httpService";
+import BreadCrumb from "./breadCrumb";
+import { setAppTitle } from "../features/titleSlice";
 
 const Image = props => {
+  const { setAppTitle } = props;
   const { photo_id: imageId } = props.match.params;
 
   const [image, setImage] = useState(null);
@@ -10,20 +15,45 @@ const Image = props => {
     http.getImage(imageId).then(setImage);
   }, [imageId]);
 
+  useEffect(() => {
+    if (image) {
+      setAppTitle(image.title);
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [image]);
+
   if (!image) {
     return null;
   }
 
   const { img, title } = image;
 
+  const breadCrumbItems = [
+    { href: "/gallery", title: "Gallery" },
+    { href: "/gallery", title: "Wedding Ideas" },
+    { title }
+  ];
+
   return (
-    <div className="thumnail">
-      <div className="bg-img">
-        <img src={`/images${img}`} alt={title}></img>
+    <>
+      <BreadCrumb items={breadCrumbItems} />
+
+      <div className="thumnail">
+        <div className="bg-img">
+          <img src={`/images${img}`} alt={title}></img>
+        </div>
+        <h3>{title}</h3>
       </div>
-      <h3>{title}</h3>
-    </div>
+    </>
   );
 };
 
-export default Image;
+const mapStateToProps = ({ gallery }) => ({
+  gallery
+});
+
+const mapDispatchToProps = dispatch => ({
+  setAppTitle: title => dispatch(setAppTitle(title))
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(Image);
