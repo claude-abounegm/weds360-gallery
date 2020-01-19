3ee3bfdb96d2f16d4effc0d4351b265405f11aec
Claude Abounegm <claude.abounegm@gmail.com>
2020-01-19 07:28:46 +0200

    finalizing image styling

--- src/components/image.jsx
+++ src/components/image.jsx
@@ -5,6 +5,17 @@ import styled from "styled-components";
 import http from "../services/httpService";
 import BreadCrumb from "./breadCrumb";
 import { setAppTitle } from "../features/titleSlice";
+import Anchor from "../elements/Anchor";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+const FontAwesomeIconBrand = ({ icon, ...props }) => (
+  <FontAwesomeIcon {...props} icon={["fab", icon]} />
+);
+
+const FacebookIcon = props => (
+  <FontAwesomeIconBrand {...props} icon="facebook-f" />
+);
+const TwitterIcon = props => <FontAwesomeIconBrand {...props} icon="twitter" />;
 
 const ImageWrapper = styled.div`
   display: flex;
@@ -28,6 +39,43 @@ const ResponsiveImg = styled.img`
   max-width: 100%;
   height: auto;
   width: 100%;
+`;
+
+const Title = styled.h2`
+  margin: 0;
+  margin-bottom: 35px;
+  font-size: 35.1px;
+  font-weight: bold;
+  color: #000;
+  line-height: 1.2;
+`;
+
+const Description = styled.h5`
+  font-family: inherit;
+  font-weight: 500;
+
+  display: block;
+  margin: 0;
+  margin-bottom: 20px;
+  font-size: 23.6px;
+  line-height: 1.42;
+  color: #000;
+
+  & a {
+    font-size: inherit;
+  }
+`;
+
+const SocialIcon = styled(FontAwesomeIconBrand)`
+  border-radius: 50%;
+  height: 34px;
+  width: 34px;
+  background-color: #000;
+  font-size: 22px;
+  padding-left: 7px;
+  padding-top: 6px;
+  color: #fff;
+  margin-right: 5px;
 `;
 
 const Image = props => {
@@ -55,7 +103,9 @@ const Image = props => {
     img,
     title,
     categoryId,
-    category: { title: categoryTitle }
+    category: { title: categoryTitle },
+    description = "Test description with some more text and more and more and more",
+    service = { name: "Al Khardawaty", href: "/en/services/151" }
   } = image;
 
   const breadCrumbItems = [
@@ -67,6 +117,9 @@ const Image = props => {
     { title }
   ];
 
+  // we use window.location here because we want the full href to be shared
+  const sharableLink = encodeURIComponent(window.location.href);
+
   return (
     <>
       <BreadCrumb items={breadCrumbItems} />
@@ -77,8 +130,29 @@ const Image = props => {
         </ImageContainer>
 
         <ImageDescription>
-          <h3>{title}</h3>
+          <Title>{title}</Title>
+          <Description>{description}</Description>
+          <Description>
+            <Anchor href={service.href}>{service.name}</Anchor>
+          </Description>
         </ImageDescription>
+
+        {/* <div class="photo--description--links">
+          <div class="photo--description--links--social">
+            <Anchor
+              href={`http://www.facebook.com/sharer.php?u=${sharableLink}`}
+            >
+              <SocialIcon icon="facebook-f" />
+            </Anchor>
+
+            <Anchor
+              target="_blank"
+              href={`https://twitter.com/share?url=${sharableLink}`}
+            >
+              <TwitterIcon />
+            </Anchor>
+          </div>
+        </div> */}
       </ImageWrapper>
     </>
   );
